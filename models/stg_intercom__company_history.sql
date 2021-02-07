e8999aad71babea9dbbcf17b53aa7299b357492e
Rob Knight <robertpknight@hotmail.com>
2021-02-07 17:30:40 +0000

    enable correct vars on tmp models

--- models/stg_intercom__company_history.sql
+++ models/stg_intercom__company_history.sql
@@ -1,3 +1,6 @@
+--To disable this model, set the using_company variable within your dbt_project.yml file to False.
+{{ config(enabled=var('using_company', True)) }}
+
 with base as (
 
     select * 
--- models/tmp/stg_intercom__company_tag_history_tmp.sql
+++ models/tmp/stg_intercom__company_tag_history_tmp.sql
@@ -1,2 +1,5 @@
+--To disable this model, set the using_company_tags variable within your dbt_project.yml file to False.
+{{ config(enabled=var('using_company_tags', True)) }}
+
 select * 
 from {{ var('company_tag_history') }}
